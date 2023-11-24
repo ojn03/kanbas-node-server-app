3879295390bf726e33a2dff24c35f5f1b6bae596
OJisMe <olivierjohnnn@gmail.com>
2023-11-23 22:21:03 -0600

    bruv

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 import "dotenv/config";
-import session from "express-session";
+// import session from "express-session";
 const app = express();
 app.use(cors());
 app.use(express.json());
