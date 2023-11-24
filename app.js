a0ad3bbbb8038991206d4da20e5dfed66ab637f2
OJisMe <olivierjohnnn@gmail.com>
2023-11-23 21:49:52 -0600

    fin a5

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from "express";
-import Hello from "./hello.js";
-import Lab5 from "./lab5.js";
+import Lab5 from "./labs/a5/lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
+import "dotenv/config";
+import session from "express-session";
 const app = express();
 app.use(cors());
 app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
 Lab5(app);
-Hello(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
