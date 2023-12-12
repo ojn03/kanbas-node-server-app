6146625f0f040f4945d320eff005af3dbf92088b
OJisMe <olivierjohnnn@gmail.com>
2023-12-11 19:50:48 -0500

    a6

--- app.js
+++ app.js
@@ -6,9 +6,34 @@ import cors from "cors";
 import "dotenv/config";
 import UserRoutes from "./users/routes.js";
 import mongoose from "mongoose";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+import session from "express-session";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(cors());
+app.use(
+	cors({
+		credentials: true,
+		origin: process.env.FRONTEND_URL
+	})
+);
+const sessionOptions = {
+	secret: "any string",
+	resave: false,
+	saveUninitialized: false
+};
+if (process.env.NODE_ENV !== "development") {
+	sessionOptions.proxy = true;
+	sessionOptions.cookie = {
+		sameSite: "none",
+		secure: true
+	};
+}
+app.use(session(sessionOptions));
+
+app.use(session(sessionOptions));
+
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
